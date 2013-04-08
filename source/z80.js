628ecba24514af44968a78f442bab19aaff6b21a
Guillaume Marty <edo999@gmail.com>
2013-04-08 11:08:43 +0100

    Expand undocumented DDCB/FDCB prefixed opcodes

--- source/z80.js
+++ source/z80.js
@@ -1657,30 +1657,198 @@ JSSMS.Z80.prototype = {
       case 0x2E: this.writeMem(location, this.sra(this.readMem(location))); break;       // SRA (IX)
       case 0x36: this.writeMem(location, this.sll(this.readMem(location))); break;       // SLL (IX) *
       case 0x3E: this.writeMem(location, this.srl(this.readMem(location))); break;       // SRL (IX)
-      case 0x46: this.bit(this.readMem(location) & BIT_0); break;                   // BIT 0,(IX)
-      case 0x4E: this.bit(this.readMem(location) & BIT_1); break;                   // BIT 1,(IX)
-      case 0x56: this.bit(this.readMem(location) & BIT_2); break;                   // BIT 2,(IX)
-      case 0x5E: this.bit(this.readMem(location) & BIT_3); break;                   // BIT 3,(IX)
-      case 0x66: this.bit(this.readMem(location) & BIT_4); break;                   // BIT 4,(IX)
-      case 0x6E: this.bit(this.readMem(location) & BIT_5); break;                   // BIT 5,(IX)
-      case 0x76: this.bit(this.readMem(location) & BIT_6); break;                   // BIT 6,(IX)
-      case 0x7E: this.bit(this.readMem(location) & BIT_7); break;                   // BIT 7,(IX)
-      case 0x86: this.writeMem(location, this.readMem(location) & ~BIT_0); break;   // RES 0,(IX)
-      case 0x8E: this.writeMem(location, this.readMem(location) & ~BIT_1); break;   // RES 1,(IX)
-      case 0x96: this.writeMem(location, this.readMem(location) & ~BIT_2); break;   // RES 2,(IX)
-      case 0x9E: this.writeMem(location, this.readMem(location) & ~BIT_3); break;   // RES 3,(IX)
-      case 0xA6: this.writeMem(location, this.readMem(location) & ~BIT_4); break;   // RES 4,(IX)
-      case 0xAE: this.writeMem(location, this.readMem(location) & ~BIT_5); break;   // RES 5,(IX)
-      case 0xB6: this.writeMem(location, this.readMem(location) & ~BIT_6); break;   // RES 6,(IX)
-      case 0xBE: this.writeMem(location, this.readMem(location) & ~BIT_7); break;   // RES 7,(IX)
-      case 0xC6: this.writeMem(location, this.readMem(location) | BIT_0); break;    // SET 0,(IX)
-      case 0xCE: this.writeMem(location, this.readMem(location) | BIT_1); break;    // SET 1,(IX)
-      case 0xD6: this.writeMem(location, this.readMem(location) | BIT_2); break;    // SET 2,(IX)
-      case 0xDE: this.writeMem(location, this.readMem(location) | BIT_3); break;    // SET 3,(IX)
-      case 0xE6: this.writeMem(location, this.readMem(location) | BIT_4); break;    // SET 4,(IX)
-      case 0xEE: this.writeMem(location, this.readMem(location) | BIT_5); break;    // SET 5,(IX)
-      case 0xF6: this.writeMem(location, this.readMem(location) | BIT_6); break;    // SET 6,(IX)
-      case 0xFE: this.writeMem(location, this.readMem(location) | BIT_7); break;    // SET 7,(IX)
+      case 0x40:
+      case 0x41:
+      case 0x42:
+      case 0x43:
+      case 0x44:
+      case 0x45:
+      case 0x46:
+      case 0x47: this.bit(this.readMem(location) & BIT_0); break;                   // BIT 0,(IX)
+      case 0x48:
+      case 0x49:
+      case 0x4A:
+      case 0x4B:
+      case 0x4C:
+      case 0x4D:
+      case 0x4E:
+      case 0x4F: this.bit(this.readMem(location) & BIT_1); break;                   // BIT 1,(IX)
+      case 0x50:
+      case 0x51:
+      case 0x52:
+      case 0x53:
+      case 0x54:
+      case 0x55:
+      case 0x56:
+      case 0x57: this.bit(this.readMem(location) & BIT_2); break;                   // BIT 2,(IX)
+      case 0x58:
+      case 0x59:
+      case 0x5A:
+      case 0x5B:
+      case 0x5C:
+      case 0x5D:
+      case 0x5E:
+      case 0x5F: this.bit(this.readMem(location) & BIT_3); break;                   // BIT 3,(IX)
+      case 0x60:
+      case 0x61:
+      case 0x62:
+      case 0x63:
+      case 0x64:
+      case 0x65:
+      case 0x66:
+      case 0x67: this.bit(this.readMem(location) & BIT_4); break;                   // BIT 4,(IX)
+      case 0x68:
+      case 0x69:
+      case 0x6A:
+      case 0x6B:
+      case 0x6C:
+      case 0x6D:
+      case 0x6E:
+      case 0x6F: this.bit(this.readMem(location) & BIT_5); break;                   // BIT 5,(IX)
+      case 0x70:
+      case 0x71:
+      case 0x72:
+      case 0x73:
+      case 0x74:
+      case 0x75:
+      case 0x76:
+      case 0x77: this.bit(this.readMem(location) & BIT_6); break;                   // BIT 6,(IX)
+      case 0x78:
+      case 0x79:
+      case 0x7A:
+      case 0x7B:
+      case 0x7C:
+      case 0x7D:
+      case 0x7E:
+      case 0x7F: this.bit(this.readMem(location) & BIT_7); break;                   // BIT 7,(IX)
+      case 0x80:
+      case 0x81:
+      case 0x82:
+      case 0x83:
+      case 0x84:
+      case 0x85:
+      case 0x86:
+      case 0x87: this.writeMem(location, this.readMem(location) & ~BIT_0); break;   // RES 0,(IX)
+      case 0x88:
+      case 0x89:
+      case 0x8A:
+      case 0x8B:
+      case 0x8C:
+      case 0x8D:
+      case 0x8E:
+      case 0x8F: this.writeMem(location, this.readMem(location) & ~BIT_1); break;   // RES 1,(IX)
+      case 0x90:
+      case 0x91:
+      case 0x92:
+      case 0x93:
+      case 0x94:
+      case 0x95:
+      case 0x96:
+      case 0x97: this.writeMem(location, this.readMem(location) & ~BIT_2); break;   // RES 2,(IX)
+      case 0x98:
+      case 0x99:
+      case 0x9A:
+      case 0x9B:
+      case 0x9C:
+      case 0x9D:
+      case 0x9E:
+      case 0x9F: this.writeMem(location, this.readMem(location) & ~BIT_3); break;   // RES 3,(IX)
+      case 0xA0:
+      case 0xA1:
+      case 0xA2:
+      case 0xA3:
+      case 0xA4:
+      case 0xA5:
+      case 0xA6:
+      case 0xA7: this.writeMem(location, this.readMem(location) & ~BIT_4); break;   // RES 4,(IX)
+      case 0xA8:
+      case 0xA9:
+      case 0xAA:
+      case 0xAB:
+      case 0xAC:
+      case 0xAD:
+      case 0xAE:
+      case 0xAF: this.writeMem(location, this.readMem(location) & ~BIT_5); break;   // RES 5,(IX)
+      case 0xB0:
+      case 0xB1:
+      case 0xB2:
+      case 0xB3:
+      case 0xB4:
+      case 0xB5:
+      case 0xB6:
+      case 0xB7: this.writeMem(location, this.readMem(location) & ~BIT_6); break;   // RES 6,(IX)
+      case 0xB8:
+      case 0xB9:
+      case 0xBA:
+      case 0xBB:
+      case 0xBC:
+      case 0xBD:
+      case 0xBE:
+      case 0xBF: this.writeMem(location, this.readMem(location) & ~BIT_7); break;   // RES 7,(IX)
+      case 0xC0:
+      case 0xC1:
+      case 0xC2:
+      case 0xC3:
+      case 0xC4:
+      case 0xC5:
+      case 0xC6:
+      case 0xC7: this.writeMem(location, this.readMem(location) | BIT_0); break;    // SET 0,(IX)
+      case 0xC8:
+      case 0xC9:
+      case 0xCA:
+      case 0xCB:
+      case 0xCC:
+      case 0xCD:
+      case 0xCE:
+      case 0xCF: this.writeMem(location, this.readMem(location) | BIT_1); break;    // SET 1,(IX)
+      case 0xD0:
+      case 0xD1:
+      case 0xD2:
+      case 0xD3:
+      case 0xD4:
+      case 0xD5:
+      case 0xD6:
+      case 0xD7: this.writeMem(location, this.readMem(location) | BIT_2); break;    // SET 2,(IX)
+      case 0xD8:
+      case 0xD9:
+      case 0xDA:
+      case 0xDB:
+      case 0xDC:
+      case 0xDD:
+      case 0xDE:
+      case 0xDF: this.writeMem(location, this.readMem(location) | BIT_3); break;    // SET 3,(IX)
+      case 0xE0:
+      case 0xE1:
+      case 0xE2:
+      case 0xE3:
+      case 0xE4:
+      case 0xE5:
+      case 0xE6:
+      case 0xE7: this.writeMem(location, this.readMem(location) | BIT_4); break;    // SET 4,(IX)
+      case 0xE8:
+      case 0xE9:
+      case 0xEA:
+      case 0xEB:
+      case 0xEC:
+      case 0xED:
+      case 0xEE:
+      case 0xEF: this.writeMem(location, this.readMem(location) | BIT_5); break;    // SET 5,(IX)
+      case 0xF0:
+      case 0xF1:
+      case 0xF2:
+      case 0xF3:
+      case 0xF4:
+      case 0xF5:
+      case 0xF6:
+      case 0xF7: this.writeMem(location, this.readMem(location) | BIT_6); break;    // SET 6,(IX)
+      case 0xF8:
+      case 0xF9:
+      case 0xFA:
+      case 0xFB:
+      case 0xFC:
+      case 0xFD:
+      case 0xFE:
+      case 0xFF: this.writeMem(location, this.readMem(location) | BIT_7); break;    // SET 7,(IX)
 
       default:
         if (DEBUG) {
