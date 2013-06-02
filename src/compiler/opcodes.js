ee05e98659400c5194e05cab6d0da592f154251b
Guillaume Marty <edo999@gmail.com>
2013-06-02 16:34:08 +0100

    Add some more opcodes and define a logic for `LD`

--- src/compiler/opcodes.js
+++ src/compiler/opcodes.js
@@ -101,36 +101,88 @@ var o = {
       return;
     }
   },
-  'LD8': function(left, right) {
-    if (left == 'nn')
-      return function(left) {
-        return n.ExpressionStatement(
-            n.AssignmentExpression('=', n.Literal(left), n.Register(right))
-        );
-      };
-    if (right == 'n')
-      return function(right) {
-        return n.ExpressionStatement(
-            n.AssignmentExpression('=', n.Register(left), n.Literal(right))
-        );
-      };
-    return function() {
-      return n.ExpressionStatement(
-          n.AssignmentExpression('=', n.Register(left), n.Register(right))
-      );
-    };
-  },
-  'LD16': function(left1, left2, right1, right2) {
-    if (left1 == 'n' && left2 == 'n')
-      return function(address) {
-        return n.ExpressionStatement(
-            n.CallExpression(address, n.Literal(value))
-        );
-      };
-    if (right1 == 'n' && right2 == 'n')
+  'LD8': function(srcRegister, dstRegister1, dstRegister2) {
+    if (dstRegister1 == undefined && dstRegister2 == undefined)
+      // Direct value assignment (ex: `LD B,n`).
       return function(value) {
         return n.ExpressionStatement(
-            n.CallExpression('set' + (left1 + left2).toUpperCase(), n.Literal(value))
+            n.AssignmentExpression('=', n.Register(srcRegister), n.Literal(value))
+        );
+      };
+    else if (dstRegister2 == undefined)
+      // Register assignment (ex: `LD B,C`).
+      return function() {
+        return n.ExpressionStatement(
+            n.AssignmentExpression('=', n.Register(srcRegister), n.Register(dstRegister1))
+        );
+      };
+    else if (dstRegister1 == 'n' && dstRegister2 == 'n')
+      // Direct address value assignment (ex: `LD A,(nn)`).
+      return function(value) {
+        return n.ExpressionStatement(
+            n.AssignmentExpression('=', n.Register(srcRegister), o.READ_MEM8(n.Literal(value)))
+        );
+      };
+    else
+      // Register address value assignment (ex: `LD A,(BC)`).
+      return function() {
+        return n.ExpressionStatement(
+            n.AssignmentExpression('=', n.Register(srcRegister),
+            o.READ_MEM8(n.CallExpression('get' + (dstRegister1 + dstRegister2).toUpperCase()))
+            )
+        );
+      };
+  },
+  'LD16': function(srcRegister1, srcRegister2, dstRegister1, dstRegister2) {
+    if (dstRegister1 == undefined && dstRegister2 == undefined)
+      // Direct value assignment (ex: `LD HL,nn`).
+      return function(value) {
+        return n.ExpressionStatement(
+            n.CallExpression('set' + (srcRegister1 + srcRegister2).toUpperCase(), n.Literal(value))
+        );
+      };
+    else if (dstRegister1 == 'n' && dstRegister2 == 'n')
+      // Direct address value assignment (ex: `LD HL,(nn)`).
+      return function(value) {
+        return n.ExpressionStatement(
+            n.CallExpression('set' + (srcRegister1 + srcRegister2).toUpperCase(), o.READ_MEM16(n.Literal(value)))
+        );
+      };
+    else
+      throw Error('Wrong parameters number');
+  },
+  'LD_WRITE_MEM': function(srcRegister1, srcRegister2, dstRegister1, dstRegister2) {
+    if (dstRegister1 == undefined && dstRegister2 == undefined)
+      // Direct value assignment (ex: `LD (HL),n`).
+      return function(value) {
+        return n.ExpressionStatement(
+            n.CallExpression('writeMem', [n.CallExpression('get' + (srcRegister1 + srcRegister2).toUpperCase()), n.Literal(value)])
+        );
+      };
+    else if (srcRegister1 == 'n' && srcRegister2 == 'n' && dstRegister2 == undefined)
+      // Direct address assignment (ex: `LD (nn),A`).
+      return function(value) {
+        return n.ExpressionStatement(
+            n.CallExpression('writeMem', [n.Literal(value), n.Register(dstRegister1)])
+        );
+      };
+    else if (srcRegister1 == 'n' && srcRegister2 == 'n')
+      // Direct address assignment (ex: `LD (nn),HL`).
+      return function(value) {
+        return [
+          n.ExpressionStatement(
+              n.CallExpression('writeMem', [n.Literal(value), n.Register(dstRegister2)])
+          ),
+          n.ExpressionStatement(
+              n.CallExpression('writeMem', [n.Literal(value + 1), n.Register(dstRegister1)])
+          )
+        ];
+      };
+    else
+      // Register assignment (ex: `LD (BC),a`).
+      return function() {
+        return n.ExpressionStatement(
+            n.CallExpression('writeMem', [n.CallExpression('get' + (srcRegister1 + srcRegister2).toUpperCase()), n.Register(dstRegister1)])
         );
       };
   },
@@ -162,6 +214,33 @@ var o = {
       );
     };
   },
+  'ADD16': function(register1, register2, register3, register4) {
+    //this.setHL(this.add16(this.getHL(), this.getBC()));
+    return function() {
+      return n.ExpressionStatement(
+          n.CallExpression('set' + (register1 + register2).toUpperCase(),
+          n.CallExpression('add16',
+          [n.CallExpression('get' + (register1 + register2).toUpperCase()),
+           n.CallExpression('get' + (register3 + register4).toUpperCase())]
+          )
+          )
+      );
+    };
+  },
+  'RLA': function() {
+    return function() {
+      return n.ExpressionStatement(
+          n.CallExpression('rla_a')
+      );
+    };
+  },
+  'RRA': function() {
+    return function() {
+      return n.ExpressionStatement(
+          n.CallExpression('rra_a')
+      );
+    };
+  },
   'JR': function(test) {
     return function(value) {
       // if (test) {
@@ -188,6 +267,14 @@ var o = {
         o.JR(n.BinaryExpression('!=', n.Register('b'), n.Literal(0)))(value)
       ];
     };
+  },
+  // Below these point, properties can't be called from outside object `n`.
+  // Move to object `o`? Mark as private?
+  'READ_MEM8': function(value) {
+    return n.CallExpression('readMem', value);
+  },
+  'READ_MEM16': function(value) {
+    return n.CallExpression('readMemWord', value);
   }
 };
 
@@ -200,12 +287,13 @@ var opcodeTable = [
   //0x01:
   {
     name: 'LD BC,nn',
-    ast: o.LD16('b', 'c', 'n', 'n'),
+    ast: o.LD16('b', 'c'),
     operand: UINT16
   },
   //0x02:
   {
-    name: 'LD (BC),A'
+    name: 'LD (BC),A',
+    ast: o.LD_WRITE_MEM('b', 'c', 'a')
   },
   //0x03:
   {
@@ -225,7 +313,7 @@ var opcodeTable = [
   //0x06:
   {
     name: 'LD B,n',
-    ast: o.LD8('b', 'n'),
+    ast: o.LD8('b'),
     operand: UINT8
   },
   //0x07:
@@ -238,11 +326,13 @@ var opcodeTable = [
   },
   //0x09:
   {
-    name: 'ADD HL,BC'
+    name: 'ADD HL,BC',
+    ast: o.ADD16('h', 'l', 'b', 'c')
   },
   //0x0A:
   {
-    name: 'LD A,(BC)'
+    name: 'LD A,(BC)',
+    ast: o.LD8('a', 'b', 'c')
   },
   //0x0B:
   {
@@ -262,7 +352,7 @@ var opcodeTable = [
   //0x0E:
   {
     name: 'LD C,n',
-    ast: o.LD8('c', 'n'),
+    ast: o.LD8('c'),
     operand: UINT8
   },
   //0x0F:
@@ -278,11 +368,13 @@ var opcodeTable = [
   //0x11:
   {
     name: 'LD DE,nn',
+    ast: o.LD16('d', 'e'),
     operand: UINT16
   },
   //0x12:
   {
-    name: 'LD (DE),A'
+    name: 'LD (DE),A',
+    ast: o.LD_WRITE_MEM('d', 'e', 'a')
   },
   //0x13:
   {
@@ -302,11 +394,13 @@ var opcodeTable = [
   //0x16:
   {
     name: 'LD D,n',
+    ast: o.LD8('d'),
     operand: UINT8
   },
   //0x17:
   {
-    name: 'RLA'
+    name: 'RLA',
+    ast: o.RLA()
   },
   //0x18:
   {
@@ -315,11 +409,13 @@ var opcodeTable = [
   },
   //0x19:
   {
-    name: 'ADD HL,DE'
+    name: 'ADD HL,DE',
+    ast: o.ADD16('h', 'l', 'd', 'e')
   },
   //0x1A
   {
-    name: 'LD A,(DE)'
+    name: 'LD A,(DE)',
+    ast: o.LD8('a', 'd', 'e')
   },
   //0x1B
   {
@@ -339,10 +435,167 @@ var opcodeTable = [
   //0x1E
   {
     name: 'LD E,n',
+    ast: o.LD8('e'),
     operand: UINT8
   },
   //0x1F
   {
-    name: 'RRA'
+    name: 'RRA',
+    ast: o.RRA()
+  },
+  //0x20
+  {
+    name: 'JR NZ,(PC+e)',
+    operand: INT8
+  },
+  //0x21
+  {
+    name: 'LD HL,nn',
+    ast: o.LD16('h', 'l'),
+    operand: UINT16
+  },
+  //0x22
+  {
+    name: 'LD (nn),HL',
+    ast: o.LD16('n', 'n', 'h', 'l'),
+    operand: UINT16
+  },
+  //0x23
+  {
+    name: 'INC HL',
+    ast: o.INC16('h', 'l')
+  },
+  //0x24
+  {
+    name: 'INC H'
+  },
+  //0x25
+  {
+    name: 'DEC H'
+  },
+  //0x26
+  {
+    name: 'LD H,n',
+    ast: o.LD8('h'),
+    operand: UINT8
+  },
+  //0x27
+  {
+    name: 'DAA'
+  },
+  //0x28
+  {
+    name: 'JR Z,(PC+e)',
+    operand: INT8
+  },
+  //0x29
+  {
+    name: 'ADD HL,HL'
+  },
+  //0x2A
+  {
+    name: 'LD HL,(nn)',
+    ast: o.LD16('h', 'l', 'n', 'n'),
+    operand: UINT16
+  },
+  //0x2B
+  {
+    name: 'DEC HL'
+  },
+  //0x2C
+  {
+    name: 'INC L'
+  },
+  //0x2D
+  {
+    name: 'DEC L'
+  },
+  //0x2E
+  {
+    name: 'LD L,n',
+    ast: o.LD8('l'),
+    operand: UINT8
+  },
+  //0x2F
+  {
+    name: 'CPL'
+  },
+  //0x30
+  {
+    name: 'JR NC,(PC+e)',
+    operand: INT8
+  },
+  //0x31
+  {
+    name: 'LD SP,nn',
+    //ast: o.LD16('sp'), // @todo Fix me.
+    operand: UINT16
+  },
+  //0x32
+  {
+    name: 'LD (nn),A',
+    ast: o.LD_WRITE_MEM('n', 'n', 'a'),
+    operand: UINT16
+  },
+  //0x33
+  {
+    name: 'INC SP'
+  },
+  //0x34
+  {
+    name: 'INC (HL)'
+  },
+  //0x35
+  {
+    name: 'DEC (HL)'
+  },
+  //0x36
+  {
+    name: 'LD (HL),n',
+    ast: o.LD_WRITE_MEM('h', 'l'),
+    operand: UINT8
+  },
+  //0x37
+  {
+    name: 'SCF'
+  },
+  //0x38
+  {
+    name: 'JR C,(PC+e)',
+    operand: INT8
+  },
+  //0x39
+  {
+    name: 'ADD HL,SP'
+  },
+  //0x3A
+  {
+    name: 'LD A,(nn)',
+    ast: o.LD8('a', 'n', 'n'),
+    operand: UINT16
+  },
+  //0x3B
+  {
+    name: 'DEC SP'
+  },
+  //0x3C
+  {
+    name: 'INC A',
+    ast: o.INC8('a')
+  },
+  //0x3D
+  {
+    name: 'DEC A',
+    ast: o.DEC8('a')
+  },
+  //0x3E
+  {
+    name: 'LD A,n',
+    ast: o.LD8('a'),
+    operand: UINT8
+  },
+  //0x3F
+  {
+    name: 'CCF'
   }
 ];
