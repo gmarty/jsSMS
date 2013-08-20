6ab670db7b0991f2cce3e4e992e4133af2629eb0
Guillaume Marty <edo999@gmail.com>
2013-08-20 22:24:47 +0100

    Refactor optimizer to make use of the module pattern

--- src/compiler/optimizer.js
+++ src/compiler/optimizer.js
@@ -28,167 +28,163 @@
  *
  * @constructor
  */
-var Optimizer = function() {
-  this.ast = [];
-};
+var Optimizer = (function() {
+  var Optimizer = function() {
+    this.ast = [];
+  };
 
-Optimizer.prototype = {
-  /**
-   * @param {Array.<Array.<Array.<Bytecode>>>} functions
-   */
-  optimize: function(functions) {
-    this.ast = functions;
+  Optimizer.prototype = {
+    /**
+     * @param {Array.<Array.<Array.<Bytecode>>>} functions
+     */
+    optimize: function(functions) {
+      this.ast = functions;
 
-    JSSMS.Utils.console.time('Optimizing');
-    for (var i = 0; i < this.ast.length; i++) {
-      this.localOptimization(i);
-    }
-    JSSMS.Utils.console.timeEnd('Optimizing');
-  },
-
-
-  /**
-   * Perform various optimizations limited to a function scope.
-   *
-   * @param {number} page
-   */
-  localOptimization: function(page) {
-    this.ast[page] = this.ast[page].map(this.inlineRegisters);
-  },
+      for (var i = 0; i < this.ast.length; i++) {
+        this.localOptimization(i);
+      }
+    },
 
 
-  /**
-   * This pass replaces references to defined registers with their respective values. Ex:
-   * ```
-   * b = 0x03;
-   * writeMem(0xFFFE, b);
-   * ```
-   *
-   * Is optimized into:
-   * ```
-   * b = 0x03;
-   * writeMem(0xFFFE, 0x03);
-   * ```
-   *
-   * It has many flaws and can be optimized:
-   *  * Inlining all getBC(), getDE()... methods will provide better efficiency.
-   *  * Limited to all registers but A and F:
-   *    * A support requires inlining of methods like `add_a`, `adc_a`, `sub_a`, `sbc_a`...
-   *    * F support requires inlining of methods like `ccf`, `add16`, `daa`, `rlc`...
-   *  * Assignment to non literal can be improved (ex: `b = b - 1` forces b to be not inlinable).
-   *
-   *  Anyway, it is just a dummy example to test the integration in the workflow.
-   *
-   * @param {Array.<Bytecode>} fn
-   * @return {Array.<Bytecode>}
-   */
-  inlineRegisters: function(fn) {
-    var definedReg = {
-      //a: false,
-      //f: false,
-      b: false,
-      c: false,
-      d: false,
-      e: false,
-      h: false,
-      l: false
-    };
-    var definedRegValue = {
-      //a: {},
-      //f: {},
-      b: {},
-      c: {},
-      d: {},
-      e: {},
-      h: {},
-      l: {}
-    };
+    /**
+     * Perform various optimizations limited to a function scope.
+     *
+     * @param {number} page
+     */
+    localOptimization: function(page) {
+      this.ast[page] = this.ast[page].map(this.inlineRegisters);
+    },
 
-    return fn.map(function(bytecodes) {
-      var ast = bytecodes.ast;
 
-      if (!ast)
-        return bytecodes;
+    /**
+     * This pass replaces references to defined registers with their respective values. Ex:
+     * ```
+     * b = 0x03;
+     * writeMem(0xFFFE, b);
+     * ```
+     *
+     * Is optimized into:
+     * ```
+     * b = 0x03;
+     * writeMem(0xFFFE, 0x03);
+     * ```
+     *
+     * It has many flaws and can be optimized:
+     *  * Inlining all getBC(), getDE()... methods will provide better efficiency.
+     *  * Limited to all registers but A and F:
+     *    * A support requires inlining of methods like `add_a`, `adc_a`, `sub_a`, `sbc_a`...
+     *    * F support requires inlining of methods like `ccf`, `add16`, `daa`, `rlc`...
+     *  * Assignment to non literal can be improved (ex: `b = b - 1` forces b to be not inlinable).
+     *
+     *  Anyway, it is just a dummy example to test the integration in the workflow.
+     *
+     * @param {Array.<Bytecode>} fn
+     * @return {Array.<Bytecode>}
+     */
+    inlineRegisters: function(fn) {
+      var definedReg = {
+        //a: false,
+        //f: false,
+        b: false,
+        c: false,
+        d: false,
+        e: false,
+        h: false,
+        l: false
+      };
+      var definedRegValue = {
+        //a: {},
+        //f: {},
+        b: {},
+        c: {},
+        d: {},
+        e: {},
+        h: {},
+        l: {}
+      };
 
-      bytecodes.ast = JSSMS.Utils.traverse(ast, function(ast) {
-        // 1st, we tag defined registers.
-        if (ast.type == 'AssignmentExpression' &&
-            ast.operator == '=' &&
-            ast.left.type == 'Register' &&
-            ast.right.type == 'Literal' &&
-            ast.left.name != 'a' &&
-            ast.left.name != 'f') {
-          definedReg[ast.left.name] = true;
-          definedRegValue[ast.left.name] = ast.right;
-        }
+      return fn.map(function(bytecodes) {
+        var ast = bytecodes.ast;
 
-        // And we make sure to tag undefined registers.
-        if (ast.type == 'AssignmentExpression' &&
-            ast.left.type == 'Register' &&
-            ast.right.type != 'Literal' &&
-            ast.left.name != 'a' &&
-            ast.left.name != 'f') {
-          definedReg[ast.left.name] = false;
-          definedRegValue[ast.left.name] = {};
-          return ast;
-        }
+        if (!ast)
+          return bytecodes;
 
-        // Then inline arguments.
-        if (ast.type == 'CallExpression') {
-          if (ast.arguments[0] &&
-              ast.arguments[0].type == 'Register' &&
-              definedReg[ast.arguments[0].name] &&
-              ast.arguments[0].name != 'a' &&
-              ast.arguments[0].name != 'f') {
-            ast.arguments[0] = definedRegValue[ast.arguments[0].name];
-            //JSSMS.Utils.console.log(ast.callee.name);
-          }
-          if (ast.arguments[1] &&
-              ast.arguments[1].type == 'Register' &&
-              definedReg[ast.arguments[1].name] &&
-              ast.arguments[1].name != 'a' &&
-              ast.arguments[1].name != 'f') {
-            ast.arguments[1] = definedRegValue[ast.arguments[1].name];
-            //JSSMS.Utils.console.log(ast.callee.name);
-          }
-          return ast;
-        }
-
-        // Inline object/array properties.
-        if (ast.type == 'MemberExpression' &&
-            ast.property.type == 'Register' &&
-            definedReg[ast.property.name] &&
-            ast.property.name != 'a' &&
-            ast.property.name != 'f') {
-          ast.property = definedRegValue[ast.property.name];
-          //JSSMS.Utils.console.log(ast.property.name);
-          return ast;
-        }
-
-        // Inline binary expressions.
-        if (ast.type == 'BinaryExpression') {
-          if (ast.right.type == 'Register' &&
-              definedReg[ast.right.name] &&
-              ast.right.name != 'a' &&
-              ast.right.name != 'f') {
-            ast.right = definedRegValue[ast.right.name];
-            //JSSMS.Utils.console.log(ast.right.name);
-            return ast;
-          }
-          if (ast.left.type == 'Register' &&
-              definedReg[ast.left.name] &&
+        bytecodes.ast = JSSMS.Utils.traverse(ast, function(ast) {
+          // 1st, we tag defined registers.
+          if (ast.type == 'AssignmentExpression' &&
+              ast.operator == '=' &&
+              ast.left.type == 'Register' &&
+              ast.right.type == 'Literal' &&
               ast.left.name != 'a' &&
               ast.left.name != 'f') {
-            ast.left = definedRegValue[ast.left.name];
-            //JSSMS.Utils.console.log(ast.right.name);
+            definedReg[ast.left.name] = true;
+            definedRegValue[ast.left.name] = ast.right;
+          }
+
+          // And we make sure to tag undefined registers.
+          if (ast.type == 'AssignmentExpression' &&
+              ast.left.type == 'Register' &&
+              ast.right.type != 'Literal' &&
+              ast.left.name != 'a' &&
+              ast.left.name != 'f') {
+            definedReg[ast.left.name] = false;
+            definedRegValue[ast.left.name] = {};
             return ast;
           }
-        }
 
-        return ast;
+          // Then inline arguments.
+          if (ast.type == 'CallExpression') {
+            if (ast.arguments[0] &&
+                ast.arguments[0].type == 'Register' &&
+                definedReg[ast.arguments[0].name] &&
+                ast.arguments[0].name != 'a' &&
+                ast.arguments[0].name != 'f') {
+              ast.arguments[0] = definedRegValue[ast.arguments[0].name];
+            }
+            if (ast.arguments[1] &&
+                ast.arguments[1].type == 'Register' &&
+                definedReg[ast.arguments[1].name] &&
+                ast.arguments[1].name != 'a' &&
+                ast.arguments[1].name != 'f') {
+              ast.arguments[1] = definedRegValue[ast.arguments[1].name];
+            }
+            return ast;
+          }
+
+          // Inline object/array properties.
+          if (ast.type == 'MemberExpression' &&
+              ast.property.type == 'Register' &&
+              definedReg[ast.property.name] &&
+              ast.property.name != 'a' &&
+              ast.property.name != 'f') {
+            ast.property = definedRegValue[ast.property.name];
+            return ast;
+          }
+
+          // Inline binary expressions.
+          if (ast.type == 'BinaryExpression') {
+            if (ast.right.type == 'Register' &&
+                definedReg[ast.right.name] &&
+                ast.right.name != 'a' &&
+                ast.right.name != 'f') {
+              ast.right = definedRegValue[ast.right.name];
+            }
+            if (ast.left.type == 'Register' &&
+                definedReg[ast.left.name] &&
+                ast.left.name != 'a' &&
+                ast.left.name != 'f') {
+              ast.left = definedRegValue[ast.left.name];
+            }
+            return ast;
+          }
+
+          return ast;
+        });
+
+        return bytecodes;
       });
+    }
+  };
 
-      return bytecodes;
-    });
-  }
-};
+  return Optimizer;
+})();
